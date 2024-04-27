cf4d20ddde1f1b094a05c8a1163d3e1ba25471d3
Kyoung Yeob <kkyub1313@gmail.com>
2024-04-27 16:00:58 +0900

    standings 시즌이동 버튼- 2
    1. 처음 api 전송하여 ejs 로드, 2.클라이언트 측에서 버튼 눌러 시즌 이동하는 각 케이스 분리, 시즌 이동 버튼 완성

--- controllers/Cstandings.js
+++ controllers/Cstandings.js
@@ -4,48 +4,91 @@ const axios = require('axios');
 
 module.exports.searchStandings = async (req, res) => {
     const leagueId = req.params.id;
-    const today = new Date();
-    let season = today.getFullYear();
-    let resultFlag = false;
-    let standings;
+    let { season } = req.query;
 
-    const options = {
-        method: 'GET',
-        url: 'https://api-football-v1.p.rapidapi.com/v3/standings',
-        params: {
-            season: season,
-            league: leagueId
-        },
-        headers: {
-            'X-RapidAPI-Key': process.env.RapidApiKey,
-            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-        }
-    };
+    if (!season) {
+        const today = new Date();
+        season = today.getFullYear();
+        let resultFlag = false;
+        let standings;
 
-    while (!resultFlag) {
-        options.params = {
-            season: season,
-            league: leagueId
+        const options = {
+            method: 'GET',
+            url: 'https://api-football-v1.p.rapidapi.com/v3/standings',
+            params: {
+                season: season,
+                league: leagueId
+            },
+            headers: {
+                'X-RapidAPI-Key': process.env.RapidApiKey,
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            }
         };
 
-        try {
-            const response = await axios.request(options);
-            if (Array.isArray(response.data.response) && response.data.response.length > 0) {
-                resultFlag = true;
-                standings = response.data.response[0];
+        while (!resultFlag) {
+            // 시즌 순위가 없는 경우, while 문 돌며 가장 최신순 standings 전송
+            options.params = {
+                season: season,
+                league: leagueId
+            };
+
+            try {
+                const response = await axios.request(options);
+                if (Array.isArray(response.data.response) && response.data.response.length > 0) {
+                    resultFlag = true;
+                    standings = response.data.response[0];
+                }
+                else {
+                    console.log(`API response for season ${season} is undefined or empty.`);
+                    season -= 1;
+                }
+            } catch (error) {
+                console.error(error);
             }
-            else {
-                console.log(`API response for season ${season} is undefined or empty.`);
-                season -= 1;
-            }
-        } catch (error) {
-            console.error(error);
         }
+
+        res.render('standing', { standings });
+
+    } else {
+        let resultFlag = false;
+        let standings;
+
+        const options = {
+            method: 'GET',
+            url: 'https://api-football-v1.p.rapidapi.com/v3/standings',
+            params: {
+                season: season,
+                league: leagueId
+            },
+            headers: {
+                'X-RapidAPI-Key': process.env.RapidApiKey,
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            }
+        };
+
+        while (!resultFlag) {
+            // 시즌 순위가 없는 경우, while 문 돌며 가장 최신순 standings 전송
+            options.params = {
+                season: season,
+                league: leagueId
+            };
+
+            try {
+                const response = await axios.request(options);
+                if (Array.isArray(response.data.response) && response.data.response.length > 0) {
+                    resultFlag = true;
+                    standings = response.data.response[0];
+                }
+                else {
+                    console.log(`API response for season ${season} is undefined or empty.`);
+                    season -= 1;
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        res.json(standings);
+
     }
-
-    console.log(standings);
-
-    //res.send(standings)
-    res.render('standing', { standings });
 }
 
--- public/js/standingScript.js
+++ public/js/standingScript.js
@@ -1,55 +1,70 @@
-  
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    // 서버에서 받은 JSON 데이터
 
-    // 테이블 생성 함수
-    function displayStandings(data) {
-      const tableContainer = document.getElementById('standings-table');
-      const table = document.createElement('table');
-      table.classList.add('table');
+let CurrSeason = season;
 
-      // 테이블 헤더 생성
-      const thead = document.createElement('thead');
-      const headerRow = document.createElement('tr');
-      // 헤더에 필요한 컬럼들 추가
-      headerRow.innerHTML = '<th>Rank</th><th>Team</th><th>Logo</th><th>Played</th><th>Win</th><th>Draw</th><th>Lose</th><th>Points</th><th>GoalsDiff</th>';
-      thead.appendChild(headerRow);
-      table.appendChild(thead);
+function displayStandings(data) {
+  const tableContainer = document.getElementById('standings-table');
+  tableContainer.innerHTML = ''; // 기존 테이블 삭제
+  const table = document.createElement('table');
+  table.classList.add('table', 'table-striped', 'table-hover');
 
-      // 테이블 바디 생성
-      const tbody = document.createElement('tbody');
-      // 데이터에서 각 팀의 정보를 가져와서 행을 생성하고 tbody에 추가
-      data.standings.forEach(team => {
-        const row = document.createElement('tr');
-        // 팀 데이터에서 필요한 정보들을 추가
-        row.innerHTML = `<td>${team.rank}</td>
-                         <td>${team.team.name}</td>
-                         <td><img src="${team.team.logo}" alt="${team.team.name}" style="max-width: 30px; max-height: 30px;"></td>
-                         <td>${team.all.played}</td>
-                         <td>${team.all.win}</td>
-                         <td>${team.all.draw}</td>
-                         <td>${team.all.lose}</td>
-                         <td>${team.points}</td>
-                         <td>${team.goalsDiff}</td>`;
-        tbody.appendChild(row);
-      });
-      table.appendChild(tbody);
+  // 테이블 헤더 생성
+  const thead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+  // 헤더에 필요한 컬럼들 추가
+  headerRow.innerHTML = '<th>Rank</th><th>Team</th><th>Logo</th><th>Played</th><th>Win</th><th>Draw</th><th>Lose</th><th>Points</th><th>GoalsDiff</th>';
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
 
-      // 테이블을 컨테이너에 추가
-      tableContainer.appendChild(table);
-    }
+  // 테이블 바디 생성
+  const tbody = document.createElement('tbody');
 
-    // standingsData를 사용하여 테이블 생성
-    displayStandings(standings);
+  // 데이터에서 마지막 배열의 정보를 가져와서 행을 생성하고 tbody에 추가
+  const lastStandings = data.league.standings[data.league.standings.length - 1];
+  lastStandings.forEach(team => {
+    const row = document.createElement('tr');
+    // 팀 데이터에서 필요한 정보들을 추가
+    row.innerHTML = `<td>${team.rank}</td>
+                       <td>${team.team.name}</td>
+                       <td><img src="${team.team.logo}" alt="${team.team.name}" style="max-width: 40px; max-height: 40px;"></td>
+                       <td>${team.all.played}</td>
+                       <td>${team.all.win}</td>
+                       <td>${team.all.draw}</td>
+                       <td>${team.all.lose}</td>
+                       <td>${team.points}</td>
+                       <td>${team.goalsDiff}</td>`;
+    tbody.appendChild(row);
   });
+  table.appendChild(tbody);
 
-  // 이전 시즌 버튼 클릭 시
+  // 테이블을 컨테이너에 추가
+  tableContainer.appendChild(table);
+}
+// 이전 시즌 버튼 클릭 시
 document.getElementById('prevYearBtn').addEventListener('click', function () {
-  changeStandingSeanson(-1);
+  changeStandingSeason(-1);
 });
 
 // 다음 시즌 버튼 클릭 시
 document.getElementById('nextYearBtn').addEventListener('click', function () {
-  changeStandingSeanson(1);
+  changeStandingSeason(1);
 });
+
+
+function updateSeasonDisplay(season) {
+  const seasonDisplay = document.getElementById('year');
+  seasonDisplay.textContent = season + ' 시즌 순위표';
+}
+
+
+function changeStandingSeason(num) {
+  CurrSeason += num;
+  axios.get(`/standings/${leagueId}?season=${CurrSeason}`)
+    .then(response => {
+      console.log(response.data);
+      console.log(response.data.league.standings);
+      displayStandings(response.data);
+      updateSeasonDisplay(response.data.league.season)
+    })
+    .catch(console.error);
+}
+
