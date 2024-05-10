1c796b83da30d6c095a765a8638bf7bb9b21cf6f
Kyoung Yeob <kkyub1313@gmail.com>
2024-05-10 14:19:48 +0900

    Cpredictions.js 배당률 혹은 예측정보가 없을 경우 로직

--- controllers/Cpredictions.js
+++ controllers/Cpredictions.js
@@ -33,7 +33,7 @@ module.exports.searchPredictions = async (req, res) => {
         // 각각의 응답은 비동기 처리를 할 이유가 없어서, Promise 객체를 꺼내기 위해 await 를 따로 한다(시간상 이득)
         const oddsResponse = await oddsResponseBefore;
         const predictionsResponse = await predictionsResponseBefore;
-        
+
 
 
         const oddsData = oddsResponse.data.response[0];
@@ -47,14 +47,18 @@ module.exports.searchPredictions = async (req, res) => {
         //     predictions: predictionsData
         // };
 
+        if (!oddsData || !predictionsData) {
+            // oddsData 또는 predictionsData가 undefined인 경우
+            throw new ExpressError();
+        } else {
+            // 템플릿 렌더링
+            res.render('prediction', { oddsData, predictionsData }); // 예시 템플릿 이름과 데이터 전달
+            //res.json(oddsData);
+        }
 
-        // 템플릿 렌더링
-        res.render('prediction', { oddsData, predictionsData }); // 예시 템플릿 이름과 데이터 전달
-        //res.json(oddsData);
     } catch (error) {
         console.error(error);
-        // 오류 처리
-        throw new ExpressError('Internal Server Error', 500);
+        throw new ExpressError('배당률 혹은 승부예측이 존재하지 않습니다! 경기시간을 기준으로 일정 시간이 경과하면 배당률 기준이 삭제될 수 있습니다.', 500);
     }
 
 
