21b032ef624cb20b6913f1ffe4a4a38018d2a55e
Kyoung Yeob <kkyub1313@gmail.com>
2024-05-13 22:58:30 +0900

    teamSquad 스키마 생성

---
+++ models/teamSquad.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PlayerSchema = new Schema({
+    id: Number,
+    name: String,
+    age: Number,
+    number: Number,
+    position: String,
+    photo: String
+});
+
+const TeamSchema = new Schema({
+    team: {
+        id: Number,
+        name: String,
+        logo: String
+    },
+    players: [{ type: Schema.Types.ObjectId, ref: 'Player' }] // 선수 정보 배열의 ObjectId 참조 
+});
+
+module.exports = mongoose.model('TeamSquad', TeamSchema);
