e6efcffed93daaa8490b6669a3398849eb70265a
Kyoung Yeob <kkyub1313@gmail.com>
2024-05-13 22:47:03 +0900

    Canalysis TEAM 컨트롤러, api 수정

--- controllers/Canalysis.js
+++ controllers/Canalysis.js
@@ -4,19 +4,25 @@ const ExpressError = require('../utils/ExpressError');
 
 module.exports.searchTeams = async (req, res) => {
     const teamId = req.params.teamId;
+    const options = {
+        method: 'GET',
+        url: 'https://api-football-v1.p.rapidapi.com/v3/teams',
+        params: { id: teamId },
+        headers: {
+            'X-RapidAPI-Key': '735987fbd7mshc0c8b4d80a39ab8p19e685jsn1cb7e725f808',
+            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+    };
 
     try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        res.json(response.data); // 예시 템플릿 이름과 데이터 전달
 
-        // 템플릿 렌더링
-        res.render('prediction', { oddsData, predictionsData }); // 예시 템플릿 이름과 데이터 전달
-        //res.json(combinedData);
     } catch (error) {
         console.error(error);
         throw new ExpressError('Internal Server Error', 500);
     }
-
-
-
 };
 
 module.exports.searchPlayers = async (req, res) => {
--- routes/analysis.js
+++ routes/analysis.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const analysis = require('../controllers/Canalysis');
 
-router.get('/teams/:id', catchAsync(analysis.searchTeams));
-router.get('/players/:id', catchAsync(analysis.searchPlayers));
+router.get('/teams/:teamId', catchAsync(analysis.searchTeams));
+router.get('/players/:playerId', catchAsync(analysis.searchPlayers));
   
 // router.get('/register', users.renderRegister);
 // router.post('/register', catchAsync(users.register));
