ae7cecb2bfa27797831c44d7216924ad37674016
Kyoung Yeob <kkyub1313@gmail.com>
2024-09-29 23:35:39 +0900

    feat(comment): make rating field optional in comment schema

--- schemas.js
+++ schemas.js
@@ -13,9 +13,9 @@ module.exports.postSchema = Joi.object({
     }).required()
 });
 
-module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
+module.exports.commentSchema = Joi.object({
+    comment: Joi.object({
+        rating: Joi.number().optional().min(1).max(5),
         body: Joi.string().required()
     }).required()
 })
